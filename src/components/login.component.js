db7d6db9b33df83fce3e728ddbba69213ba9078e
Hooddduck <hoha0309@naver.com>
2023-06-19 02:09:25 +0900

    회원가입화면 변경완료

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -137,6 +137,13 @@ class Login extends Component {
                     <span>로그인</span>
                   </button>
                 </div>
+                <div className="form-pa-reg">
+                  <div className="link-container">
+                    <a href="/password-reset">비밀번호 변경</a>
+                    <span className="vertical-line"></span>
+                    <a href="/register">회원가입</a>
+                  </div>
+                </div>
 
                 {this.state.message && (
                   <div className="form-group">
