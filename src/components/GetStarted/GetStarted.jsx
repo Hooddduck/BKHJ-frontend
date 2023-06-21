aee4542f55dbf283729c7b384b14e723d79b7c64
Hooddduck <hoha0309@naver.com>
2023-06-21 20:31:32 +0900

    메인 페이지 링크 변경

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./GetStarted.css";
 import { Routes, Route, Link } from "react-router-dom";
+import AuthService from "../../services/auth.service";
+import axios from "axios";
 
 
 const GetStarted = () => {
+  const currentUser = AuthService.getCurrentUser();
+  const [derivedValues, setDerivedValues] = useState(null);
+
+  useEffect(() => {
+    if (currentUser) {
+      axios
+        .get(`http://localhost:5000/api/customization?id=${currentUser.id}`)
+        .then((response) => {
+          setDerivedValues(response.data);
+        })
+        .catch((error) => {
+          console.error("Error:", error.response.data);
+        });
+    }
+  }, [currentUser]);
+
   return (
     <section className="g-wrapper">
       <div className="paddings innerWidth g-container">
@@ -12,9 +30,15 @@ const GetStarted = () => {
           <span className="secondaryText">
             보다 쉽고 빠른 대출 비교 서비스
           </span>
-          <button className="button">
+           {currentUser ? (
+                <button className="button">
+                  <Link to="/credit" className="linktocustomization">
+                    신용대출 비교하기
+                  </Link>
+                  </button>               ) : (
+                 <button className="button">
           <a> <Link to={"/register"}>시작하기</Link></a>
-          </button>
+          </button>              )}
         </div>
       </div>
     </section>
